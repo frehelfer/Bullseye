c86513bf1e5dfb881c3a222987839224b4371e6d
frehelfer <fre.helfer@gmail.com>
2022-10-06 11:44:48 -0300

    Initial commit

---
+++ Bullseye/App/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Frédéric Helfer on 05/10/22.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
